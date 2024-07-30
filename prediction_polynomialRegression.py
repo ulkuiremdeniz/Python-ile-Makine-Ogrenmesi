6d2a0d04143d7729c64f3148d2304273e7c2feb2
ulkuiremdeniz <ulkuiremdeniz@gmail.com>
2024-07-30 10:41:30 +0300

    polynomial regression

---
+++ prediction_polynomialRegression.py
@@ -0,0 +1,43 @@
+'''
+POLYNOMIAL REGRESSION
+    y = ax^n +bx^k+... +cx^t +k +£
+'''
+
+#KÜTÜPHANELER
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#VERİ ÖN İŞLEME
+dataset = pd.read_csv(r'C:\Users\PC\Desktop\Python ile Makine Öğrenmesi\maaslar.csv')
+print(dataset)
+
+egitim_seviyesi = dataset.iloc[:,1:2]
+maas = dataset.iloc[:,2:]
+x = egitim_seviyesi.values
+y = maas.values
+
+#linear regression ile tehmin yapmış olsaydık sonuç ne olurdu ?
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(x,y)
+
+plt.scatter(x,y,color='red')
+plt.plot(x,lin_reg.predict(x),color='blue')
+plt.show()
+
+#POLYNOMIAL REGRESSION
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)
+x_poly =poly_reg.fit_transform(x)
+print(x_poly)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(x_poly,y)
+
+plt.scatter(x,y,color='red')
+plt.plot(x,lin_reg2.predict(poly_reg.fit_transform(x)),color='blue')
+plt.show()
+
+
+print(lin_reg2.predict(poly_reg.fit_transform(11)))
+print(lin_reg2.predict(poly_reg.fit_transform(6.6)))
